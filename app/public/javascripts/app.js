b6b2b62cf732ed4c0140fb1c7a28c4f69dd07fcd
Malte <SkyGuardian42@gmail.com>
2017-09-10 03:13:36 +0200

    intitla commt

---
+++ app/public/javascripts/app.js
@@ -0,0 +1,72 @@
+  // Initialize Firebase
+var config = {
+	apiKey: "AIzaSyB2pCIqWt8_44LZKRVjNSFJNj1v8AaRnbs",
+	authDomain: "pizza-besteller.firebaseapp.com",
+	databaseURL: "https://pizza-besteller.firebaseio.com",
+	projectId: "pizza-besteller",
+	storageBucket: "",
+	messagingSenderId: "274890008242"
+	};
+firebase.initializeApp(config);
+const db = firebase.database();
+const dbRef = db.ref('validated');
+
+let app = new Vue({
+  el: '#app',
+  data: {
+		user: {
+			name: "",
+			pizza: "",
+			email: "",
+			_1109: true,
+			_1809: true	
+		},
+		anmeldungen: [],
+		formSend: false,
+		error: ""
+	},
+	methods: {
+		validateInput: function () {
+			if (this.user.name == "") {
+				this.error = "Name fehlt"
+				return true;
+			} else if (this.user.email == "") {
+				this.error = "Email fehlt"
+				return true;
+			} else if (this.user.pizza == "") {
+				this.error = "Pizzawunsch fehlt"
+				return true;
+			}
+			return false;
+		},
+		signup: function () {
+			if(this.validateInput()) return;
+
+			let opts = this.user;
+
+			const formBody = Object.keys(opts) .map(key=>encodeURIComponent(key)+'='+encodeURIComponent(opts[key])) .join('&');
+			
+			fetch('api/add', {
+				method: "POST",
+				headers: {
+					'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+					'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+				},
+				body: formBody
+			}).then(function(res) {
+				return res.json();
+			}).then(function(data) {
+				if (data.error)
+					app.error = data.error;
+				if (data.status == "success") {
+					sessionStorage.setItem('formSend', true);
+					app.formSend = true;
+				}
+			});
+		}
+	}
+})
+
+dbRef.on('value', function(snapshot) {
+	app.anmeldungen = snapshot.val();
+});
---
+++ app/routes/api.js
@@ -0,0 +1,87 @@
+const express         = require('express'),
+      admin           = require('firebase-admin'),
+      mail            = require('elasticemail'),
+      router          = express.Router(),
+      serviceAccount  = require("../serviceAccountKey.json"),
+      mailCredentials = require("../elasticemail.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://pizza-besteller.firebaseio.com"
+});
+const db = admin.database();
+
+let client = mail.createClient(mailCredentials);
+
+/* GET users listing. */
+router.post('/add', function(req, res, next) {
+  // check for valid input
+  if(!req.body.pizza && !req.body.name && !req.body.email) {
+     res.json({error: "pizzawunsch zu lang"})
+     return;
+  } else if(req.body.pizza.length > 60) {
+    res.json({error: "pizzawunsch zu lang"})
+    return;
+  }
+
+  // post to database
+  const uvRef = db.ref('unvalidated');
+  uvRef.push({
+    name: req.body.name,
+    pizza: req.body.pizza,
+    email: req.body.email,
+    _1109: req.body._1109,
+    _1809: req.body._1809,
+
+  // post to db successfull
+  }).then(function (snap) {
+    const validationId = snap.key;
+    const email = req.body.email;
+    const name = req.body.name.split(' ')[0];
+
+    // initiate email 
+    let html = `<h1 style="font-family: monospace; margin: 0">Jo ${name}, deine Bestellung bei Rolfes\' Seminarfach Pizzabesteller ist eingegangen.</h1> <h1><a style="font-family: monospace;" href="https://malts.me/api/confirm/${validationId}">Bestellung bestätigen</a></h1>`;
+    var msg = {
+      from: 'me@malts.me',
+      from_name: 'Malte\'s Pizzaservice',
+      to: email,
+      subject: 'Pizzabestellung bestätigen',
+      body_html: html
+    };
+
+    //TODO: remove DEBUG:
+    // console.log(validationId);
+    // res.json({snap: snap, val: validationId, mail: msg});
+
+    // send conformation email
+    client.mailer.send(msg, function(err, result) {
+      if (err) {
+        res.send(err);
+        return console.error(err);
+      }
+      res.json({status: "success"})
+    });
+  });
+});
+
+router.get('/confirm/:id', function(req, res, next) {
+  const uvRef = db.ref('unvalidated');
+  const valRef =  db.ref('validated'); 
+  const url = 'unvalidated/' + req.params.id;
+  console.log("'" + url + "'");
+  uvRef.child(req.params.id).once('value', function (snapshot) {
+    let data = snapshot.val();
+    if (data !== null){
+      uvRef.child(req.params.id).remove();
+      valRef.push(data);
+      res.render('validateOrder', {name: data.name.split(' ')[0]})
+    } else {
+      res.render('validationError');
+    }
+  });
+});
+
+router.get('/send/:id', function(req, res, next) {
+  
+});
+
+module.exports = router;
